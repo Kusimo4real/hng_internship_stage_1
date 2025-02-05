3d8c966def1824bc8a6a4437edcc33e451e12683
Kusimo4real <abdulsemiukusimo@gmail.com>
2025-02-05 13:22:21 +0100

    update

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+#from fastapi.middleware.cors import CORSMiddleware
 import httpx
 
 app = FastAPI()
 
-origins = ["*"]
+#origins = ["*"]
 
-app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"]
-        )
+#app.add_middleware(
+        #CORSMiddleware,
+        #allow_origins=origins,
+        #allow_credentials=True,
+        #allow_methods=["*"],
+        #allow_headers=["*"]
+        #)
 
 async def number_api(number:int):
     async with httpx.AsyncClient() as client:
